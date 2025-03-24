56925887d2961215f3efec8761be1363b88a4e0a
lacimerci <153729066+lacimerci@users.noreply.github.com>
2025-03-24 23:37:13 +0100

    hangeffekthangero

---
+++ Assets/script/SoundEffectManage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundEffectManage : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("masterVolume"))
+        {
+            PlayerPrefs.SetFloat("masterVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("masterVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("masterVolume", volumeSlider.value);
+    }
+}
